443be3fd206acf656f0d3e0aeaa4bd113df1d2aa
sonu pachauri <engg.cse12@gmail.com>
2024-03-02 18:05:10 +0530

    adding more pattern

--- dsa/pattern.py
+++ dsa/pattern.py
@@ -1,8 +1,143 @@
-# rectangular 
+# * * *
+# * * *
+# * * *
 
-n=5
-for i in range(n):
-    for j in range(n):
-        print("*",end=" ")
+# n=3
+# for i in range(n):
+#     for j in range(n):
+#         print("*",end=" ")
     
-    print()
+#     print()
+
+# --------------------------------------------------------
+
+# * 
+# * * 
+# * * *
+
+
+# n=3
+# for i in range(n):
+#     for j in range(i+1):
+#         print("*",end=" ")
+
+#     print()
+
+# --------------------------------------------------------
+# 1
+# 1 2 
+# 1 2 3
+
+# n=3
+# for i in range(n):
+#     for j in range(i+1):
+#         print(j+1,end="")
+
+#     print()
+
+# --------------------------------------------------------
+# 1
+# 2 2 
+# 3 3 3
+
+# n=3
+
+# for i in range(n):
+#     for j in range(i+1):
+#         print(i+1,end="")
+
+#     print()
+
+# --------------------------------------------------------
+# * * *
+# * *
+# *
+
+# n=3
+# for i in range(n):
+#     for j in range(n-i):
+#         print('*',end="")
+   
+#     print()
+
+# --------------------------------------------------------
+# 1 2 3
+# 1 2
+# 1
+    
+# n=3
+# for i in range(n):
+#     for j in range(n-i):
+#         print(j+1,end="")
+#     print()    
+
+# --------------------------------------------------------
+#   *
+#  ***
+# *****
+    
+# n=3
+# for i in range(n):
+#     for j in range(n-i-1):
+#         print('|',end="")
+
+#     for j in range(2*i+1):
+#         print('*',end="")
+
+#     print()  
+ 
+# --------------------------------------------------------
+# *****
+#  ***
+#   *
+
+# n=3
+# for i in range(n):
+    
+#     for j in range(i):
+#         print('|',end="")
+
+#     for j in range(2*n-2*i-1):
+#         print('*',end="")
+
+#     print()  
+
+# --------------------------------------------------------
+#   *  
+#  ***
+# ***** 
+# *****  
+#  ***
+#   * 
+    
+# n=3
+
+# for i in range(n):
+#     for j in range(n-i-1):
+#         print('|',end="")
+
+#     for j in range(2*i+1):
+#         print('*',end="")
+
+#     # for j in range(2*i+1):
+#     #     print('*',end="")
+
+#     print()
+
+# for i in range(n):
+    
+#     for j in range(i):
+#         print('|',end="")
+
+#     for j in range(2*n-2*i-1):
+#         print('*',end="")
+
+#     print()  
+    
+# --------------------------------------------------------
+#   *  
+#   **
+#   ***  
+#   **
+#   *
+
