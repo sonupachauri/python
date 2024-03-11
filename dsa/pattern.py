9231ae45fc1471c4548be32818e850834287aed2
sonu pachauri <engg.cse12@gmail.com>
2024-03-11 22:00:18 +0530

    new additon logics

--- dsa/pattern.py
+++ dsa/pattern.py
@@ -2,7 +2,7 @@
 # * * *
 # * * *
 
-# n=3
+# n=10
 # for i in range(n):
 #     for j in range(n):
 #         print("*",end=" ")
@@ -16,7 +16,7 @@
 # * * *
 
 
-# n=3
+# n=10
 # for i in range(n):
 #     for j in range(i+1):
 #         print("*",end=" ")
@@ -28,7 +28,7 @@
 # 1 2 
 # 1 2 3
 
-# n=3
+# n=10
 # for i in range(n):
 #     for j in range(i+1):
 #         print(j+1,end="")
@@ -40,7 +40,7 @@
 # 2 2 
 # 3 3 3
 
-# n=3
+# n=10
 
 # for i in range(n):
 #     for j in range(i+1):
@@ -53,7 +53,7 @@
 # * *
 # *
 
-# n=3
+# n=10
 # for i in range(n):
 #     for j in range(n-i):
 #         print('*',end="")
@@ -65,7 +65,7 @@
 # 1 2
 # 1
     
-# n=3
+# n=10
 # for i in range(n):
 #     for j in range(n-i):
 #         print(j+1,end="")
@@ -76,7 +76,7 @@
 #  ***
 # *****
     
-# n=3
+# n=10
 # for i in range(n):
 #     for j in range(n-i-1):
 #         print('|',end="")
@@ -91,7 +91,7 @@
 #  ***
 #   *
 
-# n=3
+# n=10
 # for i in range(n):
     
 #     for j in range(i):
@@ -110,7 +110,7 @@
 #  ***
 #   * 
     
-# n=3
+# n=10
 
 # for i in range(n):
 #     for j in range(n-i-1):
@@ -141,3 +141,126 @@
 #   **
 #   *
 
+# n=10
+# for i in range(n):
+#     for j in range(i+1):
+#         print('*',end="")
+        
+#     print()
+
+# for i in range(n):
+#     for j in range(n-i-1):
+#         print('*',end="")
+        
+#     print()
+
+# --------------------------------------------------------
+# 1
+# 01
+# 101
+# n=3
+
+# for i in range(n):
+#     for j in range(i+1):
+#         if((i+j)%2==0):
+#           print('1',end="")
+#         else:
+#            print('0',end="")
+        
+#     print()
+
+# --------------------------------------------------------
+# 1    1
+# 12  21
+# 123321
+
+# n=6
+
+# for i in range(n):
+#     for j in range(i+1):
+#         print(j+1,end="")
+
+#     for j in range((n-i-1)*2):
+#         print("|",end="")
+
+#     for j in range(i+1,0,-1):
+#         print(j,end="")
+
+
+#     print()
+    
+# --------------------------------------------------------
+# 1
+# 2 3
+# 4 5 6
+
+# n=5
+# k=1
+# for i in range(n):
+#     for j in range(i+1):
+#         print(k,end=" ")
+#         k +=1
+#     print()
+
+
+# --------------------------------------------------------
+# A
+# A B
+# A B C
+
+def numToChr(num):
+    return chr(num+65)
+# n=3
+
+# for i in range(n):
+#     for j in range(i+1):
+#         print(numToChr(j),end=" ")
+#     print()
+
+
+# --------------------------------------------------------
+# A B C
+# A B
+# A
+
+# n=3
+# for i in range(n):
+#     for j in range(n-i):
+#         print(numToChr(j),end=" ")
+#     print()
+
+# --------------------------------------------------------
+# A
+# B B
+# C C C
+
+# n=3
+# for i in range(n):
+#     for j in range(i+1):
+#         print(numToChr(i),end=" ")
+#     print()
+
+# --------------------------------------------------------
+#   A  
+#  ABA 
+# ABCBA
+
+# n=3
+
+# for i in range(n):
+#     for j in range(n-1-i):
+#         print(" ",end="")
+
+#     for j in range(i):
+#         print(numToChr(j),end="")
+
+#     for j in range(i,-1,-1):
+#         print(numToChr(j),end="")
+
+
+#     print()
+    
+# --------------------------------------------------------
+# C
+# B C
+# A B C
