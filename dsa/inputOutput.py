bc0b465edf5cee4024399770933ef0bfccf0100f
sonu pachauri <engg.cse12@gmail.com>
2024-03-11 23:11:40 +0530

    addition new features

---
+++ dsa/inputOutput.py
@@ -0,0 +1,33 @@
+# print('Hello')
+
+# x=5
+# # print(x)
+# y=7
+# # print(x,y,'hello')
+
+# print(x)
+# print(y)
+
+# print(x,"value of x",end="-")
+# print(y)
+
+# name=input()
+# age=int(input())
+# salary=float(input())
+# print(f'{name} and {age}')
+# print(name,type(name))
+# print(age,type(age))
+# print(salary,type(salary))
+
+# num = input().split()
+# print(num,type(num))
+
+# a,b,c = map(int,input().split())
+# print(a)
+# print(b)
+# print(c)
+
+
+lst= list(map(int,input().split()))
+print(lst)
+print(len(lst))
