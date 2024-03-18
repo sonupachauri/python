20748ac06a12540cedc8cb4f7a0d1862f224f5d8
sonu pachauri <engg.cse12@gmail.com>
2024-03-18 21:06:41 +0530

    new chnages

---
+++ dsa/dataTypes.py
@@ -0,0 +1,23 @@
+name="sonu"
+age=20
+salary=23.4
+isAdmin=True
+# set-->set()-->unique,add,remove,edit
+fruitsLiked={"orange","banana","papaya"}
+#list--->list()-->repeated,add,remove,edit
+earningFron22=[21,23,42,11,22,78]
+#Tuple---->tuple()-->fixed,repea
+earningbefore22=(22,34,12)
+#dictionary--->key value pair
+property={"car":3,"bikes":10,"house":12}
+overhyped=None
+
+print(name,type(name))
+print(age,type(age))
+print(salary,type(salary))
+print(isAdmin,type(isAdmin))
+print(fruitsLiked,type(fruitsLiked))
+print(earningFron22,type(earningFron22))
+print(earningbefore22,type(earningbefore22))
+print(property,type(property))
+print(overhyped,type(overhyped))
